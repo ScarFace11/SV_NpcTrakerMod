54c51131ce61a539c7a0e56512e71aa542c41c78
Scar_Face <man-jack@bk.ru>
2026-07-19 19:52:23 +0300

    рефакторинг, изменена логика рисования плиток, добавлена возможность имзенить назначение клавиш, добавлено отслеживание по времени

---
+++ NpcTrackerMod/ModConfig.cs
@@ -0,0 +1,17 @@
+using StardewModdingAPI;
+
+
+namespace NpcTrackerMod
+{
+    /// <summary>
+    /// Конфигурация мода, сохраняемая в config.json.
+    /// </summary>
+    public class ModConfig
+    {
+        /// <summary> Клавиша открытия меню трекера. </summary>
+        public SButton MenuKey { get; set; } = SButton.G;
+
+        /// <summary> Клавиша отладочного вывода варпов. </summary>
+        public SButton DebugKey { get; set; } = SButton.Z;
+    }
+}
